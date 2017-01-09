4bd4fad11184931e0d4c5097ef9e39367a7cd898
kdiluca <kristen@mapzen.com>
2017-01-09 13:09:19 -0500

    add POST request function, replaced string time with in time in file requests, fixed spacing, minor file fixes

--- py/data_importer.py
+++ py/data_importer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 '''
-If you're running this from this directory you can start it with some incantation such as:
-PYTHONPATH=$PYTHONPATH:../../tools.libs ./data_importer.py ../../conf/manila.json localhost:8002
+If you're running this from this directory you can create the json request files from the raw CSV data by running this:
+PYTHONPATH=PYTHONPATH:../../tools/.libs ./data_importer.py
+
+That will create a reporter_requests.json file.
+Run the following to curl the reporter requests thru the reporter_service via POST:
+time cat reporter_requests.json | parallel --progress -j7 curl -s --data '{}' -o /dev/null http://localhost:8002/segment_match?
 '''
 
 import sys
@@ -16,7 +20,6 @@ import valhalla
 import csv,json,requests
 from operator import itemgetter
 from datetime import datetime, date
-import urllib
 
 '''
 sample url looks like this:
@@ -35,147 +38,35 @@ fieldnames = ("time","vehicleId","lat","lon")
 reader = csv.DictReader(csvfile, fieldnames=None)
 prior_vehicle_id = 0
 for row in sorted(reader, key=itemgetter(fieldnames[1], fieldnames[0])):
-    epoch = datetime(1970,1,1)
-    i = datetime.strptime(row.get(fieldnames[0]),"%Y-%m-%dT%H:%M:%S.%fZ")
-    delta_time = int((i - epoch).total_seconds())
-    start_time = delta_time
-    if (prev != 0):
-        elapsed_time += delta_time - prev
-    else:
-        elapsed_time += delta_time - start_time
-    prev = start_time
-    #TODO: Need to replace times with int time
-    #row.update((fieldnames[0],delta_time) for fieldnames[0] in row)
-    
-    if prior_vehicle_id == 0:
-        # First row - start a new trace URL
-        jsonfile.write('http://localhost:8002/segment_match?json={"trace":[')
-        json.dump(row, jsonfile)
-    elif row.get(fieldnames[1]) == prior_vehicle_id:
-        # Continuation of same vehicle Id - add to current URL
-        jsonfile.write(',')
-        json.dump(row, jsonfile)
-    else:
-        # End the prior URL. Ideally we would send a URL to the reporter...
-        jsonfile.write(']}' + '\n')
-        # Start a new URL
-        jsonfile.write('http://localhost:8002/segment_match?json={"trace":[')
-        json.dump(row, jsonfile)
-    # Update prior_vehicle Id
-    prior_vehicle_id = row.get(fieldnames[1])
+  epoch = datetime(1970,1,1)
+  i = datetime.strptime(row.get(fieldnames[0]),"%Y-%m-%dT%H:%M:%S.%fZ")
+  delta_time = int((i - epoch).total_seconds())
+  start_time = delta_time
+  if (prev != 0):
+    elapsed_time += delta_time - prev
+  else:
+    elapsed_time += delta_time - start_time
+  prev = start_time
+  #Replace times with int time
+  row['time'] = delta_time
+
+  if prior_vehicle_id == 0:
+    # First row - start a new trace URL
+    jsonfile.write('{"trace":[')
+    json.dump(row, jsonfile, separators=(',',':'))
+  elif row.get(fieldnames[1]) == prior_vehicle_id:
+    # Continuation of same vehicle Id - add to current URL
+    jsonfile.write(',')
+    json.dump(row, jsonfile, separators=(',',':'))
+  else:
+    # End the prior URL. Ideally we would send a URL to the reporter...
+    jsonfile.write(']}\n')
+    # Start a new URL
+    jsonfile.write('{"trace":[')
+    json.dump(row, jsonfile, separators=(',',':'))
+  # Update prior_vehicle Id
+  prior_vehicle_id = row.get(fieldnames[1])
  
 # Close off the last URL (ideally send to reporter)
 jsonfile.write(']}')
 jsonfile.close()
-
-
-#TODO: what do do with last few traces that total to < 60 sec?
-
-
-#enable threaded server
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
-
-#TODO: send json requests to segment matcher instead of to request file
-
-#custom handler for getting routes
-class SegmentMatcherHandler(BaseHTTPRequestHandler):
-    def __init__(self, *args):
-        self.segment_matcher = valhalla.SegmentMatcher()
-        BaseHTTPRequestHandler.__init__(self, *args)
-
-    #parse the request because we dont get this for free!
-    def handle_request(self):
-        self.path = urllib.urlopen(url)
-        #split the query from the path
-        try:
-            split = urlparse.urlsplit(self.path)
-        except:
-			raise Exception('Try a url that looks like /action?query_string')
-		#path has the costing method and action in it
-        try:
-            action = actions[split.path.split('/')[-1]]
-        except:
-            raise Exception('Try a valid action: ' + str([k for k in actions]))
-		#get a dict and unexplode non-list entries
-        params = urlparse.parse_qs(split.query)
-        for k,v in params.iteritems():
-			if len(v) == 1:
-				params[k] = v[0]
-		#save jsonp or not
-        jsonp = params.get('jsonp', None)
-        if params.has_key('json'):
-			params = json.loads(params['json'])
-        if jsonp is not None:
-		    params['jsonp'] = jsonp
-
-        result = self.segment_matcher.Match(json.dumps(params))
-
-        #javascriptify this
-        if jsonp:
-            result = jsonp + '=' + result + ';'
-        #hand it back
-        return result, jsonp is not None 
-
-    #send a success
-    def succeed(self, response, jsonp):
-        self.send_response(200)
-	
-		#set some basic info
-        self.send_header('Access-Control-Allow-Origin','*')
-        if jsonp:
-            self.send_header('Content-type', 'text/plain;charset=utf-8')
-        else:
-            self.send_header('Content-type', 'application/json;charset=utf-8')
-            self.send_header('Content-length', len(response))
-            self.end_headers()
-	
-		#hand it back
-        self.wfile.write(response)
-	
-	#send a fail
-    def fail(self, error):
-        self.send_response(400)
-
-    #set some basic info
-        self.send_header('Access-Control-Allow-Origin','*')
-        self.send_header('Content-type', 'text/plain;charset=utf-8')
-        self.send_header('Content-length', len(error))
-        self.end_headers()
-	
-    #hand it back
-        self.wfile.write(str(error))
-	
-	#handle the request
-    def do_GET(self):
-	    #get out the bits we care about
-	    try:
-	    	response, jsonp = self.handle_request()
-	      	self.succeed(response, jsonp)
-	    except Exception as e:
-	      	self.fail(str(e))
-
-#go off and wait for connections
-if __name__ == '__main__':
-  #check for a config file
-  conf = {}
-  try:
-    with open(sys.argv[1]) as f:
-      conf = json.load(f)
-    valhalla.Configure(sys.argv[1])
-    server = sys.argv[2].split('/')[-1].split(':')
-    server[1] = int(server[1])
-    server = tuple(server)
-  except Exception as e:
-    sys.stderr.write('Problem with config file: {0}\n'.format(e)) 
-    sys.exit(1)
-
-  #setup the server
-  SegmentMatcherHandler.protocol_version = 'HTTP/1.0'
-  httpd = ThreadedHTTPServer(server, SegmentMatcherHandler)
-
-  try:
-    httpd.serve_forever()
-  except KeyboardInterrupt:
-    httpd.server_close()
-
--- py/reporter_service.py
+++ py/reporter_service.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 '''
-If you're running this from this directory you can start it with some incantation such as:
-PYTHONPATH=$PYTHONPATH:../../tools.libs ./reporter_service.py ../../conf/manila.json localhost:8002
+If you're running this from this directory you can start the server with the following command:
+PYTHONPATH=PYTHONPATH:../../tools/.libs ./reporter_service.py ../../conf/manila.json localhost:8002
 '''
 
 import sys
@@ -39,57 +39,55 @@ elapsed_time = 0
 
 #loop thru nodes
 for node in tree.iter(): 
-	#store name string
-	if 'name' in node.tag:
-		name = ElementTree.tostring(node, encoding='utf8', method='text')
-		#namestr = '"name":' + name.strip() + ','
-		namestr = name.strip()
+  #store name string
+  if 'name' in node.tag:
+  name = ElementTree.tostring(node, encoding='utf8', method='text')
+  #namestr = '"name":' + name.strip() + ','
+  namestr = name.strip()
 
-	#store lat lon
-	if 'lat' in node.attrib:
-		lat = node.attrib.get('lat')
-	if 'lon' in node.attrib:
-		lon = node.attrib.get('lon')
+  #store lat lon
+  if 'lat' in node.attrib:
+  lat = node.attrib.get('lat')
+  if 'lon' in node.attrib:
+  lon = node.attrib.get('lon')
 
-    	#convert UTC to timestamp to datetime integer values of seconds
-	if 'time' in node.tag:
-		timestamp = ElementTree.tostring(node, encoding='utf8', method='text')
-		epoch = datetime(1970,1,1)
-		i = datetime.strptime(timestamp.strip(),"%Y-%m-%dT%H:%M:%SZ")
-		delta_time = int((i - epoch).total_seconds())
-		start_time = delta_time
-		if (prev != 0):
-			elapsed_time += delta_time - prev
-		else:
-			elapsed_time += delta_time - start_time
+    #convert UTC to timestamp to datetime integer values of seconds
+  if 'time' in node.tag:
+  timestamp = ElementTree.tostring(node, encoding='utf8', method='text')
+  epoch = datetime(1970,1,1)
+  i = datetime.strptime(timestamp.strip(),"%Y-%m-%dT%H:%M:%SZ")
+  delta_time = int((i - epoch).total_seconds())
+  start_time = delta_time
+  if (prev != 0):
+    elapsed_time += delta_time - prev
+  else:
+    elapsed_time += delta_time - start_time
 
-		prev = start_time
+  prev = start_time
 
-		#create json string from gpx traces and split out requests into approx. 60 second chunks based on elapsed time
-		#jsonstr = {"lat":lat,"lon":lon,"time":str(delta_time)},
-		jsonstr = '{"lat":' + lat + ',"lon":' + lon + ',"time":' + str(delta_time) +'},'
-		if (elapsed_time > 60):
-		    jsonstr = jsonstr[:-1]
-		json_list.append(jsonstr)
+  #create json string from gpx traces and split out requests into approx. 60 second chunks based on elapsed time
+  #jsonstr = {"lat":lat,"lon":lon,"time":str(delta_time)},
+  jsonstr = '{"lat":' + lat + ',"lon":' + lon + ',"time":' + str(delta_time) +'},'
+  if (elapsed_time > 60):
+  jsonstr = jsonstr[:-1]
+  json_list.append(jsonstr)
 
-		if (elapsed_time > 60):
-			gpx_list.append('{"trace":[')
-			#gpx_list.append(namestr)
-			gpx_list.extend(json_list)
-			gpx_list.append(']}')
-			gpx_list.extend('\n')
+  if (elapsed_time > 60):
+    gpx_list.append('{"trace":[')
+    #gpx_list.append(namestr)
+    gpx_list.extend(json_list)
+    gpx_list.append(']}')
+    gpx_list.extend('\n')
 
-			outfile = open('/home/kdiluca/sandbox/reporter/py/output/gpx_%s.json' %namestr, 'w')
-			for line in gpx_list:
-				#outfile.write(line)
-				json.dumps(line, outfile)
-			
-			json_list = []
-			outfile.close()
-			elapsed_time = 0
+    outfile = open('/home/kdiluca/sandbox/reporter/py/output/gpx_%s.json' %namestr, 'w')
+    for line in gpx_list:
+  #outfile.write(line)
+  json.dumps(line, outfile)
+
+    json_list = []
+    outfile.close()
+    elapsed_time = 0
 """
-#TODO: what do do with last few traces that total to < 60 sec?
-    
 
 #enable threaded server
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
@@ -98,95 +96,108 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 #custom handler for getting routes
 class SegmentMatcherHandler(BaseHTTPRequestHandler):
 
-	def __init__(self, *args):
-		self.segment_matcher = valhalla.SegmentMatcher()
-		BaseHTTPRequestHandler.__init__(self, *args)
+  def __init__(self, *args):
+    self.segment_matcher = valhalla.SegmentMatcher()
+    BaseHTTPRequestHandler.__init__(self, *args)
 
-    #parse the request because we dont get this for free!
-	def handle_request(self):
-		#split the query from the path
-		try:
-			split = urlparse.urlsplit(self.path)
-		except:
-			raise Exception('Try a url that looks like /action?query_string')
-		#path has the costing method and action in it
-		try:
-			action = actions[split.path.split('/')[-1]]
-		except:
-		  	raise Exception('Try a valid action: ' + str([k for k in actions]))
-		#get a dict and unexplode non-list entries
-		params = urlparse.parse_qs(split.query)
-		for k,v in params.iteritems():
-			if len(v) == 1:
-				params[k] = v[0]
-		#save jsonp or not
-		jsonp = params.get('jsonp', None)
-		if params.has_key('json'):
-			params = json.loads(params['json'])
-		if jsonp is not None:
-			params['jsonp'] = jsonp
+  #parse the request because we dont get this for free!
+  def handle_request(self,post):
+    #split the query from the path
+    try:
+      split = urlparse.urlsplit(self.path)
+    except:
+      raise Exception('Try a url that looks like /action?query_string')
+    #path has the costing method and action in it
+    try:
+      action = actions[split.path.split('/')[-1]]
+    except:
+      raise Exception('Try a valid action: ' + str([k for k in actions]))
+    #get a dict and unexplode non-list entries
+    params = urlparse.parse_qs(split.query)
+    for k,v in params.iteritems():
+      if len(v) == 1:
+        params[k] = v[0]
+    #save jsonp or not
+    jsonp = params.get('jsonp', None)
+    if params.has_key('json'):
+      params = json.loads(params['json'])
+    if jsonp is not None:
+      params['jsonp'] = jsonp
+    #handle POST
+    if post:
+      params = json.loads(self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8'))
+    print params
 
-		
-		result = self.segment_matcher.Match(json.dumps(params))
+    result = self.segment_matcher.Match(json.dumps(params))
 
-		"""
-		trace_file = '/home/kdiluca/sandbox/reporter/py/gpx_'+namestr+'.json'
-		with open(trace_file, 'r') as trace:		
- 			for line in trace:		
- 				result = self.segment_matcher.Match(json.dumps(line))
-		"""
+    """
+    trace_file = '/home/kdiluca/sandbox/reporter/py/gpx_'+namestr+'.json'
+    with open(trace_file, 'r') as trace:
+       for line in trace:
+     result = self.segment_matcher.Match(json.dumps(line))
+    """
 
-		#prints segments array info to terminal in csv format if partial start and end are false
-		segments_dict = json.loads(result)
-		for seg in segments_dict['segments']:
-		    if "false" in seg['partial_start'] and "false" in seg['partial_end']:
-		        print ','.join([seg['segment_id'], seg['begin_time'], seg['end_time'], seg['length']])
-		    sys.stdout.flush()
+    #prints segments array info to terminal in csv format if partial start and end are false
+    segments_dict = json.loads(result)
+    for seg in segments_dict['segments']:
+      if "false" in seg['partial_start'] and "false" in seg['partial_end']:
+        print ','.join([seg['segment_id'], seg['begin_time'], seg['end_time'], seg['length']])
+        sys.stdout.flush()
 
-				
-		#javascriptify this
-		if jsonp:
-			result = jsonp + '=' + result + ';'
-		#hand it back
-		return result, jsonp is not None 
 
-	#send a success
-	def succeed(self, response, jsonp):
-		self.send_response(200)
-	
-		#set some basic info
-		self.send_header('Access-Control-Allow-Origin','*')
-		if jsonp:
-			self.send_header('Content-type', 'text/plain;charset=utf-8')
-		else:
-			self.send_header('Content-type', 'application/json;charset=utf-8')
-	    	self.send_header('Content-length', len(response))
-	    	self.end_headers()
-	
-		#hand it back
-		self.wfile.write(response)
-	
-	#send a fail
-	def fail(self, error):
-	    self.send_response(400)
-	
-	    #set some basic info
-	    self.send_header('Access-Control-Allow-Origin','*')
-	    self.send_header('Content-type', 'text/plain;charset=utf-8')
-	    self.send_header('Content-length', len(error))
-	    self.end_headers()
-	
-	    #hand it back
-	    self.wfile.write(str(error))
-	
-	#handle the request
-	def do_GET(self):
-	    #get out the bits we care about
-	    try:
-	    	response, jsonp = self.handle_request()
-	      	self.succeed(response, jsonp)
-	    except Exception as e:
-	      	self.fail(str(e))
+    #javascriptify this
+    if jsonp:
+      result = jsonp + '=' + result + ';'
+    #hand it back
+    return result, jsonp is not None
+
+  #send a success
+  def succeed(self, response, jsonp):
+    self.send_response(200)
+
+    #set some basic info
+    self.send_header('Access-Control-Allow-Origin','*')
+    if jsonp:
+      self.send_header('Content-type', 'text/plain;charset=utf-8')
+    else:
+      self.send_header('Content-type', 'application/json;charset=utf-8')
+      self.send_header('Content-length', len(response))
+    self.end_headers()
+
+    #hand it back
+    self.wfile.write(response)
+
+  #send a fail
+  def fail(self, error):
+    self.send_response(400)
+
+    #set some basic info
+    self.send_header('Access-Control-Allow-Origin','*')
+    self.send_header('Content-type', 'text/plain;charset=utf-8')
+    self.send_header('Content-length', len(error))
+    self.end_headers()
+
+    #hand it back
+    self.wfile.write(str(error))
+
+  #handle the request
+  def do_GET(self):
+    #get out the bits we care about
+    try:
+      response, jsonp = self.handle_request(False)
+      self.succeed(response, jsonp)
+    except Exception as e:
+      self.fail(str(e))
+
+  #handle the request
+  def do_POST(self):
+    #get out the bits we care about
+    try:
+      response, jsonp = self.handle_request(True)
+      self.succeed(response, jsonp)
+    except Exception as e:
+      self.fail(str(e))
+
 
 #go off and wait for connections
 if __name__ == '__main__':
