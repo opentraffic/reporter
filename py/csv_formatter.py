910f6fe867ef535e940a99caa35221515ad5a48e
Kevin Kreiser <kevinkreiser@gmail.com>
2017-02-17 14:37:55 -0500

    add uuid to formatter

--- py/csv_formatter.py
+++ py/csv_formatter.py
@@ -10,18 +10,19 @@ import sys
 import csv
 import json
 from operator import itemgetter
-from datetime import datetime, date
+import time
+import calendar
 
 # Read the file
 with open(sys.argv[1], 'r') as csvfile:
   columns = ("time","vehicleId","lat","lon")
   reader = csv.DictReader(csvfile, fieldnames=columns)
-  epoch = datetime(1970,1,1)
   trace = []
+  uuid = 0
   # For each row of data
   for row in sorted(reader, key=itemgetter(columns[1], columns[0])):
     # Convert to epoch seconds
-    row[columns[0]] = int((datetime.strptime(row.get(columns[0]),"%Y-%m-%dT%H:%M:%S.%fZ") - epoch).total_seconds())
+    row[columns[0]] = calendar.timegm(time.strptime(row.get(columns[0]),"%Y-%m-%dT%H:%M:%S.%fZ"))
     # These shouldn't be strings
     row['lon'] = float(row['lon'])
     row['lat'] = float(row['lat'])
@@ -32,6 +33,7 @@ with open(sys.argv[1], 'r') as csvfile:
     # End the prior vehicle
     else:
       if len(trace):
-        print json.dumps({'trace':trace}, separators=(',',':'))
-      #print json.dumps({'type': 'Feature', 'geometry': { 'type': 'LineString', 'coordinates': [ [i['lon'], i['lat']] for i in trace ] }, 'properties':{}}, separators=(',',':')), ','
+        print json.dumps({'uuid': uuid, 'trace':trace}, separators=(',',':'))
+        uuid += 1
+        #print json.dumps({'type': 'Feature', 'geometry': { 'type': 'LineString', 'coordinates': [ [i['lon'], i['lat']] for i in trace ] }, 'properties':{'uuid':uuid}}, separators=(',',':')), ','
       trace = [ row ]
