1845594255d9bbf01b53b6acab8204b91c084f29
Kevin Kreiser <kevinkreiser@gmail.com>
2017-05-15 12:46:38 -0400

    try docker cp hopefully ok even though exec wasnt

--- tests/circle.sh
+++ tests/circle.sh
@@ -52,7 +52,8 @@ sleep 3
 # for now we'll use file mode, later we'll switch to consume from kafka
 #
 echo "Running flink job from file..."
-sudo lxc-attach -n "$(docker inspect --format "{{.Id}}" jobmanager)" -- bash -c /opt/flink/bin/flink run -c opentraffic.accumulator.Accumulator /jobs/$(basename ${flink_job}).jar --file valhalla_data/*.csv --reporter http://reporter:${reporter_port}/report?
+docker cp "${flink_job}" "$(docker inspect --format "{{.Id}}" jobmanager)":/job.jar
+sudo lxc-attach -n "$(docker inspect --format "{{.Id}}" jobmanager)" -- bash -c /opt/flink/bin/flink run -c opentraffic.accumulator.Accumulator /job.jar --file valhalla_data/*.csv --reporter http://reporter:${reporter_port}/report?
 
 # test that we got data through to the echo server
 # TODO: this is lame do something more meaningful
