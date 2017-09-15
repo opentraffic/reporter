a7130f95d2129710470a14c8e90a255190c1e3af
Kevin Kreiser <kevinkreiser@gmail.com>
2017-09-15 11:00:37 -0400

    add bounding box an dmore loging to data ingest

--- py/make_requests.sh
+++ py/make_requests.sh
@@ -10,14 +10,14 @@ set -e
 
 function usage {
   echo -e "Usage:\n-s s3 bucket url\n-f regex to use with grep to get interesting files\n" 1>&2
-  echo "Example: AWS_DEFAULT_PROFILE=opentraffic $0 -s s3://heaps_of_data/2016_11/ -f 2016_11_01.*gz -b 172.17.0.1:9092 -t mytopic -w 300" 1>&2
+  echo "Example: AWS_DEFAULT_PROFILE=opentraffic $0 -s s3://heaps_of_data/2016_11/ -f 2016_11_01.*gz -b 172.17.0.1:9092 -t mytopic -w 300 -x 14.5,120.9,14.7,121.1" 1>&2
   echo "Note: bucket listing is not recursive" 1>&2
   echo "Note: data is pipe delimited: date|id|x|x|x|x|x|x|x|lat|lon|x|x with date format: %Y-%m-%d %H:%M:%S" 1>&2
   exit 1
 }
 
 timeout=0
-while getopts ":s:f:b:t:w:" opt; do
+while getopts ":s:f:b:t:w:x:" opt; do
   case $opt in
     s) s3_dir=$(echo ${OPTARG} | sed -e 's@/\?$@/@g')
     ;;
@@ -29,6 +29,8 @@ while getopts ":s:f:b:t:w:" opt; do
     ;;
     w) timeout="${OPTARG}"
     ;;
+    x) bbox="${OPTARG}"
+    ;;
     \?) echo "Invalid option -${OPTARG}" 1>&2 && usage
     ;;
   esac
@@ -37,6 +39,13 @@ done
 hash=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 key_with='lambda line: line.split("|")[1]'
 value_with="lambda line: re.sub(r'^([^\\|]+)\\|', r'\\1|${hash}', line)"
+if [ ${bbox+x} ]; then
+  minx=$(echo ${bbox} | awk -F, '{print $1}')
+  miny=$(echo ${bbox} | awk -F, '{print $2}')
+  maxx=$(echo ${bbox} | awk -F, '{print $3}')
+  maxy=$(echo ${bbox} | awk -F, '{print $4}')
+  send_if="lambda l: partial(lambda c: c[0] > ${minx} and c[0] < ${maxx} and c[1] > ${miny} and c[1] < ${maxy}, [float(x) for x in l.split(\"|\")[9:11]])()"
+fi
 
 if [[ -z ${s3_dir} ]] || [[ -z ${file_re} ]] || [[ -z ${bootstrap} ]] || [[ -z ${topic} ]]; then
   echo "Missing required option" 1>&2 && usage
@@ -51,7 +60,12 @@ for file in ${files}; do
   #download in the foreground
   echo "Retrieving ${file} from s3" && aws s3 cp ${s3_dir}${file} . &> /dev/null
   #send to kafka producer
-  zcat ${file} | sort | ./cat_to_kafka.py --bootstrap ${bootstrap} --topic ${topic} --key-with "${key_with}" --value-with "${value_with}" -
+  if [ ${send_if+x} ]; then
+    echo "zcat ${file} | ./cat_to_kafka.py --bootstrap ${bootstrap} --topic ${topic} --key-with \"${key_with}\" --value-with \"${value_with}\" --send-if \"${send_if}\" -"
+    zcat ${file} | ./cat_to_kafka.py --bootstrap ${bootstrap} --topic ${topic} --key-with "${key_with}" --value-with "${value_with}" --send-if "${send_if}" -
+  else
+    zcat ${file} | ./cat_to_kafka.py --bootstrap ${bootstrap} --topic ${topic} --key-with "${key_with}" --value-with "${value_with}" -
+  fi
   #wait around a little for the system to process it
   sleep ${timeout}
   #done with this
