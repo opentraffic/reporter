19951eab816e7a2daf0a5c9b332a4e66ce61ce6a
Kevin Kreiser <kevinkreiser@gmail.com>
2017-09-22 18:54:06 -0400

    make segment object smaller and simpler since we dont combine them anymore

--- src/main/java/io/opentraffic/reporter/Segment.java
+++ src/main/java/io/opentraffic/reporter/Segment.java
@@ -16,33 +16,18 @@ public class Segment implements Comparable<Segment>{
   public static long INVALID_SEGMENT_ID = 0x3fffffffffffL;
   public long id;         //main segment id
   public long next_id;    //optional next, could be invalid
-  public long min, max;   //epoch seconds
-  public int duration;    //epoch seconds
+  public double min, max; //epoch seconds
   public int length;      //meters
   public int queue;       //meters
-  public int count;       //how many
-  public static final int SIZE = 8 + 8 + 8 + 8 + 4 + 4 + 4 + 4;
+  public static final int SIZE = 8 + 8 + 8 + 8 + 4 + 4;
   
   public Segment(long id, Long next_id, double start, double end, int length, int queue) {
     this.id = id;
     this.next_id = next_id == null ? INVALID_SEGMENT_ID : next_id;
-    this.min = (long)Math.floor(start);
-    this.max = (long)Math.ceil(end);
-    this.duration = (int)Math.round(end - start);
+    this.min = start;
+    this.max = end;
     this.length = length;
     this.queue = queue;
-    this.count = 1;
-  }
-  
-  public Segment(long id, Long next_id, long min, long max, int duration, int length, int queue, int count) {
-    this.id = id;
-    this.next_id = next_id == null ? INVALID_SEGMENT_ID : next_id;
-    this.min = min;
-    this.max = max;
-    this.duration = duration;
-    this.length = length;
-    this.queue = queue;
-    this.count = 1;
   }
   
   //first 3 bits are hierarchy level then 22 bits of tile id. the rest we want zero'd out
@@ -51,7 +36,7 @@ public class Segment implements Comparable<Segment>{
   }
   
   public boolean valid() {
-    return count > 0 && min > 0 && max > 0 && duration > 0 && length > 0 && queue >= 0;
+    return min > 0 && max > 0 && max > min && length > 0 && queue >= 0;
   }
   
   @Override
@@ -77,12 +62,13 @@ public class Segment implements Comparable<Segment>{
     if(next_id != INVALID_SEGMENT_ID)
       buffer.append(next_id);
     buffer.append(',');
-    buffer.append(Integer.toString(duration)); buffer.append(',');
-    buffer.append(Integer.toString(count)); buffer.append(',');
+    Double duration = new Double(Math.round(max-min));
+    buffer.append(Integer.toString(duration.intValue())); buffer.append(',');
+    buffer.append("1,"); //count
     buffer.append(Integer.toString(length)); buffer.append(',');
     buffer.append(Integer.toString(queue)); buffer.append(',');
-    buffer.append(Long.toString(min)); buffer.append(',');
-    buffer.append(Long.toString(max)); buffer.append(',');
+    buffer.append(Long.toString(new Double(Math.floor(min)).longValue())); buffer.append(',');
+    buffer.append(Long.toString(new Double(Math.ceil(max)).longValue())); buffer.append(',');
     buffer.append(source); buffer.append(',');
     //TODO: parse this in the formatting processor or get it as a program argument
     buffer.append("AUTO");
@@ -97,17 +83,15 @@ public class Segment implements Comparable<Segment>{
     public static void put(ByteBuffer buffer, Segment s) {
       buffer.putLong(s.id);
       buffer.putLong(s.next_id);
-      buffer.putLong(s.min);
-      buffer.putLong(s.max);
-      buffer.putInt(s.duration);
+      buffer.putDouble(s.min);
+      buffer.putDouble(s.max);
       buffer.putInt(s.length);
       buffer.putInt(s.queue);
-      buffer.putInt(s.count);
     }
     
     public static Segment get(ByteBuffer buffer) {
-      return new Segment(buffer.getLong(), buffer.getLong(), buffer.getLong(), buffer.getLong(), 
-          buffer.getInt(), buffer.getInt(), buffer.getInt(), buffer.getInt());
+      return new Segment(buffer.getLong(), buffer.getLong(), buffer.getDouble(), buffer.getDouble(), 
+          buffer.getInt(), buffer.getInt());
     }
 
     public Serializer<Segment> serializer() {
